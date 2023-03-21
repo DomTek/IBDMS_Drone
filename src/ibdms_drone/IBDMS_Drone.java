2055f191c84396db92ae0bb1159c44abc538f9f4
DomTek <dominikjutzi@gmail.com>
2023-03-21 21:16:23 +1000

    Basic draught that will drastically change but gets me a start and help think the project through.

--- src/ibdms_drone/IBDMS_Drone.java
+++ src/ibdms_drone/IBDMS_Drone.java
@@ -4,6 +4,8 @@
  */
 package ibdms_drone;
 
+import java.util.Scanner;
+
 /**
  *
  * @author H529780
@@ -14,7 +16,19 @@ public class IBDMS_Drone {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        //Creates scanner to take input of the System by the user and accept an ID and name for the drone
+        Scanner inScan = new Scanner(System.in);
+        System.out.println("please enter the Drone ID: ");
+        int droneId = inScan.nextInt();
+        System.out.println("Please enter a name for the Drone: ");
+        String droneName = inScan.nextLine();
+        
+        
+        Drone[] drone = new Drone[1];
+        Drone drones1 = new Drone(droneId, droneName, 0,0 );
+        drone[0] = drones1;
+        
     }
     
 }
